3e8388a50bf8276298293856688d9451c7c865b1
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-08-30 17:29:25 +0200

    :wheelchair: Fallback to 4000

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import Server from 'application/http/server';
 
-const server = new Server(parseInt(process.env.PORT as string));
+const server = new Server(parseInt(process.env.PORT as string) || 4000);
 
 server.start();

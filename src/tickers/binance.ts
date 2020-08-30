e01b5838b500ecc65b4309829179899e74aa310c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-08-30 20:47:30 +0200

    :sparkles: Save data to redis

--- src/tickers/binance.ts
+++ src/tickers/binance.ts
@@ -1,10 +1,12 @@
+import redis from 'redis';
 import WebSocket from 'ws';
 
 const url = 'wss://fstream.binance.com/stream?streams=btcusdt@markPrice';
 const connection = new WebSocket(url);
+const redisClient = redis.createClient();
 
 connection.onmessage = (e: { data: any }) => {
-  const data = JSON.parse(e.data);
+  const { data } = JSON.parse(e.data);
 
-  console.log({ data });
+  redisClient.hset('ticker', 'binance', data.p);
 };

2fb09b347bf22ba2b3c08d62f938f7806a196782
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-08-30 21:38:44 +0200

    :sparkles: Print raw btc value

--- src/http/request-handlers/api/root.ts
+++ src/http/request-handlers/api/root.ts
@@ -1,8 +1,23 @@
 import { Request, Response } from 'express';
+import redis from 'redis';
+import { promisify } from 'util';
+
+const redisClient = redis.createClient();
+const getAsync = promisify(redisClient.hgetall).bind(redisClient);
 
 const root = async (_req: Request, res: Response): Promise<void> => {
+  const data = await getAsync('ticker');
+
+  const values = Object.values(data);
+  const price = (
+    values.reduce(
+      (total: number, value: string) => total + parseFloat(value),
+      0,
+    ) / values.length
+  ).toFixed(2);
+
   res.setHeader('content-type', 'text/plain');
-  res.send('soon');
+  res.send(price);
 };
 
 export default root;

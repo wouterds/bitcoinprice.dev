7363421b52f9b0fe2904be4da1771c6fb3e66eb3
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-08-30 21:26:00 +0200

    :construction: Coinbase ticker

---
+++ src/tickers/coinbase.ts
@@ -0,0 +1,31 @@
+import redis from 'redis';
+import WebSocket from 'ws';
+
+const url = 'wss://ws-feed.pro.coinbase.com';
+const connection = new WebSocket(url);
+const redisClient = redis.createClient();
+
+connection.on('open', () => {
+  connection.send(
+    JSON.stringify({
+      type: 'subscribe',
+      product_ids: ['BTC-USD'],
+      channels: [
+        {
+          name: 'ticker',
+          product_ids: ['BTC-USD'],
+        },
+      ],
+    }),
+  );
+});
+
+connection.onmessage = (e: { data: any }) => {
+  const data = JSON.parse(e.data);
+
+  const price = data?.price || null;
+
+  if (price) {
+    redisClient.hset('ticker', 'coinbase', price);
+  }
+};

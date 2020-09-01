6c5162f99d483b17179ee446430f931e718f349e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-09-01 23:34:30 +0200

    :bug: Bind to redisclient

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -4,8 +4,8 @@ import { promisify } from 'util';
 const redisClient = Redis.createClient({ host: 'redis' });
 
 const get = promisify(redisClient.get).bind(redisClient);
-const set = redisClient.set;
-const hset = redisClient.hset;
+const set = redisClient.set.bind(redisClient);
+const hset = redisClient.hset.bind(redisClient);
 const hgetall = promisify(redisClient.hgetall).bind(redisClient);
 
 const redis = {

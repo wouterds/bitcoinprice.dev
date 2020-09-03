09960b0dada0efff8923f9620b292c8c75c1d77f
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-09-03 20:50:11 +0200

    :wrench: Take minutely average samples

--- .docker/node/entrypoint.sh
+++ .docker/node/entrypoint.sh
@@ -10,6 +10,9 @@ yarn ticker:kraken > /proc/1/fd/1 2>&1 &
 # wait for tickers to connect
 sleep 10
 
+# take minutely average sample asap
+yarn minutely-average:sample
+
 # start cron deamon
 crond
 

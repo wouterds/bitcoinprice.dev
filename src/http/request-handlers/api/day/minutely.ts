780243ef3535caf9191c137314427e1d7e58786a
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-09-03 22:00:48 +0200

    :bug: Order was wrong

--- src/http/request-handlers/api/day/minutely.ts
+++ src/http/request-handlers/api/day/minutely.ts
@@ -6,7 +6,7 @@ const repository = new TickerRepository();
 const root = async (_req: Request, res: Response): Promise<void> => {
   const averages = await repository.getMinutelyAverages();
   const values = Object.entries(averages)
-    .sort((a, b) => (a[0] > b[0] ? -1 : 1))
+    .sort((a, b) => (a[0] > b[0] ? 1 : -1))
     .map((item) => item[1]);
 
   res.setHeader('content-type', 'text/plain');

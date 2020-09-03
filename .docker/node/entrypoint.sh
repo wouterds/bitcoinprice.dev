f0950c332b2edbd5ae30f95b3833e5fc4e37ecd2
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-09-03 08:14:57 +0200

    :wrench: Wait for tickers to start

--- .docker/node/entrypoint.sh
+++ .docker/node/entrypoint.sh
@@ -7,6 +7,9 @@ yarn start:ticker:bitstamp > /proc/1/fd/1 2>&1 &
 yarn start:ticker:coinbase > /proc/1/fd/1 2>&1 &
 yarn start:ticker:kraken > /proc/1/fd/1 2>&1 &
 
+# wait for tickers to connect
+sleep 10
+
 # start cron deamon
 crond
 

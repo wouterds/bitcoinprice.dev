be560178dd150fa88ffd2778244fb6ac4bb82603
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-09-03 20:43:08 +0200

    :sparkles: Save minutely average

---
+++ src/scripts/minutely-average-sample.ts
@@ -0,0 +1,19 @@
+import { roundToNearestMinutes } from 'date-fns';
+import TickerRepository from 'repositories/ticker';
+import Sources from 'tickers/sources';
+
+const sources = Object.values(Sources);
+
+(async () => {
+  const repository = new TickerRepository();
+
+  const time = roundToNearestMinutes(new Date()).getTime() / 1000;
+  const average = await repository.getAveragePriceForSources(sources);
+  if (!average) {
+    process.exit(1);
+  }
+
+  repository.addMinutelyAverage(time.toString(), average);
+
+  process.exit(0);
+})();

5f4db4d0c0f95eaba8a87388ff6be0df232ee604
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-09-03 08:14:37 +0200

    :wrench: Start tickers directly on launch

--- .docker/node/entrypoint.sh
+++ .docker/node/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# start tickers
+yarn start:ticker:binance > /proc/1/fd/1 2>&1 &
+yarn start:ticker:bitfinex > /proc/1/fd/1 2>&1 &
+yarn start:ticker:bitstamp > /proc/1/fd/1 2>&1 &
+yarn start:ticker:coinbase > /proc/1/fd/1 2>&1 &
+yarn start:ticker:kraken > /proc/1/fd/1 2>&1 &
+
+# start cron deamon
 crond
 
+# start application
 yarn start

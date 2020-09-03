78d0fbd4f96a418d1b08c4f36ef5f8d99319cb79
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2020-09-03 09:08:22 +0200

    :sparkles: getAveragePriceForSources

--- src/repositories/ticker.ts
+++ src/repositories/ticker.ts
@@ -21,6 +21,33 @@ class TickerRepository {
 
     redis.set(`ticker.${source}`, price, 'EX', this.ttl);
   };
+
+  getAveragePriceForSources = async (
+    sources: Sources[],
+  ): Promise<string | null> => {
+    const prices: string[] = [];
+    for (const source of sources) {
+      const price = await this.getPriceForSource(source);
+      if (!price) {
+        continue;
+      }
+
+      prices.push(price);
+    }
+
+    if (!prices.length) {
+      return null;
+    }
+
+    const average = (
+      prices.reduce(
+        (total: number, value: string) => total + parseFloat(value),
+        0,
+      ) / prices.length
+    ).toFixed(2);
+
+    return average;
+  };
 }
 
 export default TickerRepository;
